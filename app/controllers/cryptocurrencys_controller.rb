2601f5b61e4a2aaa97e139bee0dd84ab296f9696
avneesh1mehta <avneesh1mehta@hotmail.com>
2017-11-13 17:14:55 -0800

    Fixed model error

--- app/controllers/cryptocurrencys_controller.rb
+++ app/controllers/cryptocurrencys_controller.rb
@@ -1,18 +1,18 @@
 class CryptocurrencysController < ApplicationController
   def fetch
-    cryptocurrency = Cryptocurrency.find_by_id(params[:id])
-    if cryptocurrency.name == 'btc'
+    cryptocurrency = Cryptocurrency.find_by name: 'btc'
+    if cryptocurrency.name == "btc"
       response = Faraday.get "https://api.coinmarketcap.com/v1/ticker/bitcoin/"
-      response = JSON.parse(response.body)
+      response = JSON.parse(response.body)[0]
       @name = "Bitcoin"
     else
       response = Faraday.get "https://api.coinmarketcap.com/v1/ticker/ethereum/"
-      response = JSON.parse(response.body)
+      response = JSON.parse(response.body)[0]
       @name = "Ethereum"
     end
-    @price = response["price_usd"]
-    @marketcap = response["market_cap_usd"]
-    @volume = response["24h_volume_usd"]
+    @price = response["price_usd"].to_f
+    @marketcap = response["market_cap_usd"].to_f
+    @volume = response["24h_volume_usd"].to_f
     cryptocurrency.update(marketcap: @marketcap, price: @price, volume: @volume)
   end
 end
