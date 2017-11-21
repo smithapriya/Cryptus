04fc6b76ac272aee553b5b03f022ecd3affd87e7
Emma Tao <jiayue_tao@berkeley.edu>
2017-11-21 14:27:38 -0800

    user model: profile access through id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     # Force users to login before doing anything.
     before_action :authenticate_user!
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # root_url is needed for Devise Gem.
   # root to: "home#index"
   root "home#index"
   resources :users
-  get "users/show", to: "users#show", as: "profile"
-  get "profile" => redirect("users/show")
+  get "users/:name", to: "users#show", as: "profile"
+  #get "profile" => redirect("users/show")
   get "*path" => redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
