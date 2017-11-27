e16658d6f7809674ac208998519d48dcb4f3c967
Emma Tao <jiayue_tao@berkeley.edu>
2017-11-26 22:11:47 -0800

    both currencies displayed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,20 +3,24 @@ class HomeController < ApplicationController
   def index
     fetch
   end
+  
   def fetch
-    cryptocurrency = Cryptocurrency.find_by name: "btc"
-    if cryptocurrency.name == "btc"
-      response = Faraday.get "https://api.coinmarketcap.com/v1/ticker/bitcoin/"
-      response = JSON.parse(response.body)[0]
-      @name = "Bitcoin"
-    else
-      response = Faraday.get "https://api.coinmarketcap.com/v1/ticker/ethereum/"
-      response = JSON.parse(response.body)[0]
-      @name = "Ethereum"
-    end
-    @price = response["price_usd"].to_f
-    @marketcap = response["market_cap_usd"].to_f
-    @volume = response["24h_volume_usd"].to_f
-    cryptocurrency.update(marketcap: @marketcap, price: @price, volume: @volume)
+    bitcoin = Cryptocurrency.find_by name: "btc"
+    response = Faraday.get "https://api.coinmarketcap.com/v1/ticker/bitcoin/"
+    response = JSON.parse(response.body)[0]
+    @bname = "Bitcoin"
+    @bprice = response["price_usd"].to_f
+    @bmarketcap = response["market_cap_usd"].to_f
+    @bvolume = response["24h_volume_usd"].to_f
+    bitcoin.update(marketcap: @bmarketcap, price: @bprice, volume: @bvolume)
+
+    ethereum = Cryptocurrency.find_by name: "eth"
+    response = Faraday.get "https://api.coinmarketcap.com/v1/ticker/ethereum/"
+    response = JSON.parse(response.body)[0]
+    @ename = "Ethereum"
+    @eprice = response["price_usd"].to_f
+    @emarketcap = response["market_cap_usd"].to_f
+    @evolume = response["24h_volume_usd"].to_f
+    ethereum.update(marketcap: @emarketcap, price: @eprice, volume: @evolume)
   end
 end
